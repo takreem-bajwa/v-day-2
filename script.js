b0793114bb134c77bf396456ace3d2912cdaf680
Roshan Kumar Sah <roshansah393@gmail.com>
2026-02-07 14:09:19 +0545

    js files updated

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ btn.addEventListener("mouseover",movebutton);
 function movebutton(){
     const x = Math.random() * (window.innerWidth - btn.offsetWidth);
     const y = Math.random() * (window.innerHeight - btn.offsetHeight);
-    console.log(x);
-    console.log(y);
+    // console.log(x);
+    // console.log(y);
     btn.style.position = "absolute";
     btn.style.transition = "left 0.4s ease, top 0.4s ease";
     btn.style.left = x + "px";
